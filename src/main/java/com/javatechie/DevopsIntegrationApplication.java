bc356cf965febb4b2c68108331fd7468db6bc14f
mohammad rafi shaik <rafitest444@hotmail.com>
2024-08-21 20:23:14 +0300

    image pull policy-Always

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome my DevOps World -hey there howdy";
+		return "Welcome my DevOps World -hey there howdy2";
 	}
 
 	public static void main(String[] args) {
